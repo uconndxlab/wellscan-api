60a33c35d2d1ba29de8a9af3fdb56a2b74bcfd55
Renoj Varghese <renoj.varghese@uconn.edu>
2019-02-09 14:08:55 -0500

    converting to ES6

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-const path = require('path');
-const express = require('express');
+
+import express from "express";
 
 const app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.send("Hello World!");
 })
 
+app.get("/api/:system/:category/:barcode", (req, res) => {
+    });
 
+});
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log(`Listening to localhost:${PORT}...`);
     console.log('Ctrl+C to quit.');
 })
