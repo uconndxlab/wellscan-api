746c1dfa1a49c690017dfba629386cd9539c3676
Renoj Varghese <renoj.varghese@uconn.edu>
2019-02-12 16:50:13 -0500

    bug fix for string checks

--- src/operators/string.js
+++ src/operators/string.js
@@ -1,4 +1,4 @@
 export default {
   "first-ingredient" : (ingred, search) => ingred.toLowerCase().startsWith(search.toLowerCase()),
-  "contains": (ingred, search) => ingred.toLowerCase().contains(search.toLowerCase())
+  "contains": (ingred, search) => ingred.toLowerCase().includes(search.toLowerCase())
 }
