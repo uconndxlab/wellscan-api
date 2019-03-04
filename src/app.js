f2a79ec20f4e057fec45ceadf98294ab66f1602d
Renoj Varghese <renoj.varghese@uconn.edu>
2019-03-03 21:03:53 -0500

    updating codebase; adding comments

--- src/app.js
+++ src/app.js
@@ -25,59 +25,97 @@ app.get("/api/:system/:category/:barcode", (req, res, next) => {
       }
     );
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+app.get("/api/:system/:category/:barcode", (req, res, next) => {
+  //get nutritionix data from barcode
+  nix.getNutritionByUPC(req.params.barcode,
+    nutrition => {
+      res.locals.nutrition = nutrition.data;
+      next();
+    },
+    err => {
+      res
+        .status(404)
+        .send("Could not find nutrition information for " + params.barcode);
+    }
+  );
+});
+
+app.get("/api/:system/:category/:barcode", (req, res, next) => {
+  //check if system and category are valid before comparing nutrition information
+  const system = db[req.params.system];
+  if (!system) {
+    res.status(400).send(req.params.system + " ranking system not found");
+  }
+
+  const category = system["categories"][req.params.category];
+  if (!category) {
+    res.status(400).send(req.params.category + " category not found in " + system);
+  }
+
+  res.locals.rankingInfo = {
+    defaultRank: system["default-rank"],
+    values: system.values,
+    category: category
+  }
+
+  next();
+});
+
+app.get("/api/:system/:category/:barcode", (req, res, next) => {
+  // analyzing nutrition information and providing a rank to the food
+  let nutrition = res.locals.nutrition;
+  let category = res.locals.rankingInfo.category;
+
+  category.forEach((check, idx) => {
+    let requirements = check.requirements;
+    let i = 0;
+    let pass = true;
+
+    // loop through requirements to see if we can find one test that fails
+    // if one test fails, then this is note the right rank for the the food; stop the loop
+    while ( i < requirements.length && pass) {
+      let r = requirements[i];
+      let operator = operators[r.operator];
+      pass = pass && operator(nutrition[r.property], r.value);
+      i++;
+    }
+
+    // if food passed all the requirements
+    // set it to ranking, which is the actual string name
+    if (pass){
+      res.locals.rank = check.rank;
+      next();
+    }
+  })
+  // if the food didn't meet any of the ranks, it belongs in the the default rank
+  res.locals.rank = res.locals.rankingInfo.defaultRank;
+  next();
+});
+
 app.get("/api/:system/:category/:barcode", (req, res) => {
-    let rank = null;
+  let {nutrition, rankingInfo, rank } = res.locals;
 
-    const rankSys = db[req.params.system];
-    if (!rankSys) {
-      res.status(400).send("ranking system not found");
-    }
+  // the data sent back as response
+  let data = {
+    rank,
+    value: rankingInfo.values[rank],
+    name: nutrition["item_name"],
+    system: req.params.system,
+    category: req.params.category,
+    barcode: req.params.barcode,
+  }
 
-    const tests = rankSys["categories"][req.params.category];
-    if (!tests) {
-      res.status(400).send(req.params.category + " category not found in " + rankSys["name"] + " ranking system");
-    }
-
-    let nut = req.nut.data;
-    for (let i = 0; i < tests.length && !rank; i++) {
-      let current = tests[i];
-      let requirements = current["requirements"];
-      let pass = true;
-      requirements.forEach(r => {
-          let operator = operators[r.operator];
-          pass = pass && operator(nut[r.property], r.value);
-      })
-      if (pass) {
-        let rankName =  current.ranking;
-        rank = {
-          name: rankName,
-          value: rankSys["values"][rankName]
-        }
-      }
-    }
-
-    if (!rank) {
-      let rankName = rankSys.default;
-      rank = rank = {
-        name: rankName,
-        value: rankSys["values"][rankName]
-      }
-    }
-
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(JSON.stringify(rank));
-
-
-
-
-});
-
+  res.status(200).send(data);
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Listening to localhost:${PORT}...`);
-    console.log('Ctrl+C to quit.');
-})
+  console.log(`Listening to localhost:${PORT}...`);
+  console.log("Ctrl+C to quit.");
+});
 
 export default app;
