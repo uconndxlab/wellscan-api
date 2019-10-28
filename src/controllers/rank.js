65e923d7847eca6fe17145bb605238ff0068d480
Renoj Varghese <renoj.varghese@uconn.edu>
2019-10-28 18:00:19 -0400

    getting unit tests to work again

--- src/controllers/rank.js
+++ src/controllers/rank.js
@@ -1,43 +1,44 @@
 import express from "express";
 const router = express.Router();
 
-import Nutritionix from "../helpers/nutritionix";
+import Nutritionix from "../nutrition-sources/nutritionix";
 const nix = new Nutritionix();
 
-import WellScanGlobal from "../helpers/wellscanglobal";
-const wsg = new WellScanGlobal();
+// import WellScanGlobal from "../helpers/wellscanglobal";
+// const wsg = new WellScanGlobal();
 
 import operators from "../operators";
 import db from "../db";
-import { red } from "ansi-colors";
-import { deflateRawSync } from "zlib";
+// import { red } from "ansi-colors";
+// import { deflateRawSync } from "zlib";
 
 
-router.get("/api/getFoodInfo/:barcode", (req, res) => {
-  res.set("Access-Control-Allow-Origin", "*")
+// router.get("/api/getFoodInfo/:barcode", (req, res) => {
+//   res.set("Access-Control-Allow-Origin", "*")
 
-  // Check if we already have the food, in case we need to use this information later.
-  wsg.checkFoodExists(req.params.barcode, 
-    ret => {
-      var d = {};
-      if(ret.found) {
-        d = ret.data.data();
-        d.status = 200;
-        d.msg = "Product found in WellSCAN Global";
-      } else {
-        d.status = 404;
-        d.msg = "Product not found in WellSCAN Global";
-      }
-      res.status(200).send(d);
-    },
-    err => {
-      res
-        .status(401)
-        .send("Failed to contact WellSCAN Global: " + err.message);
-    });
-});
+//   // Check if we already have the food, in case we need to use this information later.
+//   wsg.checkFoodExists(req.params.barcode, 
+//     ret => {
+//       var d = {};
+//       if(ret.found) {
+//         d = ret.data.data();
+//         d.status = 200;
+//         d.msg = "Product found in WellSCAN Global";
+//       } else {
+//         d.status = 404;
+//         d.msg = "Product not found in WellSCAN Global";
+//       }
+//       res.status(200).send(d);
+//     },
+//     err => {
+//       res
+//         .status(401)
+//         .send("Failed to contact WellSCAN Global: " + err.message);
+//     });
+// });
 
 router.get("/api/:system/:category/:barcode", (req, res, next) => {
+  
   res.set("Access-Control-Allow-Origin", "*")
   //check if system and category are valid before comparing nutrition information
   const system = db[req.params.system];
@@ -51,29 +52,29 @@ router.get("/api/:system/:category/:barcode", (req, res, next) => {
     res.status(400).send(req.params.category + " category not found in " + system);
     return;
   }
-
+  next();
   // Check if we already have the food, in case we need to use this information later.
   // Note: This will matter when we want to avoid external calls to nutrition sources.
-  wsg.checkFoodExists(req.params.barcode, 
-    ret => {
-      res.locals.alreadyFound = ret.found;
+  // wsg.checkFoodExists(req.params.barcode, 
+  //   ret => {
+  //     res.locals.alreadyFound = ret.found;
 
-      res.locals.rankingInfo = {
-        defaultRank: system["default-rank"],
-        values: system.values,
-        category: category
-      }
-      next();
-    },
-    err => {
-      res
-        .status(401)
-        .send("Failed to contact WellSCAN Global: " + err.message);
-    });
+  //     res.locals.rankingInfo = {
+  //       defaultRank: system["default-rank"],
+  //       values: system.values,
+  //       category: category
+  //     }
+  //     next();
+  //   },
+  //   err => {
+  //     res
+  //       .status(401)
+  //       .send("Failed to contact WellSCAN Global: " + err.message);
+  //   });
 });
 
 router.get("/api/:system/:category/:barcode", (req, res, next) => {
-
+  
     //get nutritionix data from barcode
     let opt = {
       barcode: req.params.barcode,
@@ -97,7 +98,9 @@ router.get("/api/:system/:category/:barcode", (req, res, next) => {
 router.get("/api/:system/:category/:barcode", (req, res, next) => {
     // analyzing nutrition information and providing a rank to the food
     let nutrition = res.locals.nutrition;
-    let category = res.locals.rankingInfo.category;
+    //let category = res.locals.rankingInfo.category || req.query.category;
+    let system = db[req.params.system]
+    let category = system["categories"][req.params.category];
 
     category.forEach((check, idx) => {
       let requirements = check.requirements;
@@ -118,22 +121,25 @@ router.get("/api/:system/:category/:barcode", (req, res, next) => {
       // set it to ranking, which is the actual string name
       if (pass){
         res.locals.rank = check.rank;
+        res.locals.value = system["values"][check.rank];
         next();
       }
     })
     // if the food didn't meet any of the ranks, it belongs in the the default rank
-    res.locals.rank = res.locals.rankingInfo.defaultRank;
+    
+    res.locals.rank = system["default-rank"];
+    res.locals.value = system["values"][res.locals.rank]
     next();
    
 });
 
 router.get("/api/:system/:category/:barcode", (req, res) => {
-  let {nutrition, rankingInfo, rank } = res.locals;
-
+  let {nutrition, value, rank } = res.locals;
+  //console.log(res.locals)
   // the data sent back as response
   let data = {
     rank,
-    value:rankingInfo.values[rank],
+    value,
     name: nutrition["item_name"],
     system: req.params.system,
     category: req.params.category,
@@ -141,24 +147,24 @@ router.get("/api/:system/:category/:barcode", (req, res) => {
   }
  
   // prepare the record that will get added to WellSCAN Global
-  var fbrecord = {
-    item_name:data.name,
-    upc:data.barcode,
-    rankings:{}
-  }
-  // more preppy stuffz
-  fbrecord.rankings[data.system] = {
-    rank:rank,
-    category:req.params.category,
-  }
+  // var fbrecord = {
+  //   item_name:data.name,
+  //   upc:data.barcode,
+  //   rankings:{}
+  // }
+  // // more preppy stuffz
+  // fbrecord.rankings[data.system] = {
+  //   rank:rank,
+  //   category:req.params.category,
+  // }
   
-  wsg.addRecord(fbrecord);
+  // wsg.addRecord(fbrecord);
 
-  if(res.locals.alreadyFound)
-    data.msg = "Note: The food was already in WellSCAN Global. Updated as appropriate.";
-  else {
-    data.msg = "The food was not already in WellSCAN Global, but has been added."
-  }
+  // if(res.locals.alreadyFound)
+  //   data.msg = "Note: The food was already in WellSCAN Global. Updated as appropriate.";
+  // else {
+  //   data.msg = "The food was not already in WellSCAN Global, but has been added."
+  // }
   res.status(200).send(data);
 });
 
