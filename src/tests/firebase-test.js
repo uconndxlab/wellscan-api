b545522b1d4471c81f48433ff6a292c23731b6ab
Renoj Varghese <renoj.varghese@uconn.edu>
2019-12-17 17:36:07 -0500

    updating test for fatsecret

--- src/tests/firebase-test.js
+++ src/tests/firebase-test.js
@@ -4,44 +4,51 @@ const wsg = new WellScanGlobal();
 let g = 0;
 let m = 0;
 let f = 0;
-wsg.getAllFoods(docs => {
-        docs.forEach(doc => {
+(() => {wsg.getAllFoods(docs => {
+        docs.forEach(async function(doc) {
             let fbdata = doc.data();
             let system = "swap"
             let category = fbdata.rankings[system].category === "grain" ? "grain-whole" : fbdata.rankings[system].category
+            if (!category) { return; }
             let rank = fbdata.rankings[system].rank
             let barcode = fbdata.upc;
             let url = `http://localhost:8080/api/${system}/${category}/${barcode}`;
-            axios.get(url).then( res => {
+            let result = await axios.get(url).then( res => {
                 console.log(url);
                 if (res.data.rank == rank) {
 
                     console.log("    SUCCESS:", barcode, rank)
+                    g++;
                 }
                 else {
-                    console.log("    FAIL:", barcode, "Expected", rank, "and got", res.data.rank)
+                    console.log("    MEHHHH:", barcode, "Expected", rank, "and got", res.data.rank)
                     wsg.updateFoodRecord({
                         upc: barcode,
                         different_swap_rank: true,
                         original_swap_rank: rank,
                         now_swap_rank: res.data.rank
                     })
+                    m++;
                 }
-
+                console.log(g,m,f)
                 
             }).catch( err => {
                 console.log(url);
                 if (err.response) {
-                    console.log("    OTHER:", barcode, err.response.data.status, err.response.data.msg);
+                    console.log("    FAIL:", barcode, err.response.data.status, err.response.data.msg);
                 }
                 else {
                     console.log("    UNKNOWN ERROR");
                 }
+                f++;
+                console.log(g,m,f)
             })
-        })
+            return result;
+        })  
     },
     e => {
         console.log(e);
     },
     300
 )
+})();
